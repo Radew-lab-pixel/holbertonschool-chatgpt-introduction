023303db8b0dc07335d1fe370f5faebf565a777b
OEM Configuration (temporary user) <ktmqliu@gmail.com>
2025-01-13 12:27:26 +1100

    Amend factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,10 +1,19 @@
+#!/usr/bin/python3
 import sys
 
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
+        n -= 1
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
+if len(sys.argv) > 1:
+    try:
+        num = int(sys.argv[1])
+        f = factorial(num)
+        print(f)
+    except ValueError:
+        print("Please provide a valid integer.")
+else:
+    print("Please provide a number as an argument.")
