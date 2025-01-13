c9539a7b9b80242fc28bc8ea84e4cafb3eeac35d
OEM Configuration (temporary user) <ktmqliu@gmail.com>
2025-01-13 14:10:01 +1100

    Amend print_arguments.py

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-for i in range(len(sys.argv)):
-    print(sys.argv[i])
-# End-of-file (EOF)
+# Check if arguments are passed, excluding the script name
+if len(sys.argv) > 1:
+    print("Arguments passed to the script:")
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print(f"Argument {i}: {arg}")
+else:
+    print("No additional arguments passed.")
